77ca84f527da707457625b99234eb62e6cf77977
Newway1997 <573491019@qq.com>
2020-11-27 22:15:27 +0800

    yes

--- check.js
+++ check.js
@@ -1,20 +1,25 @@
 const execSync = require("child_process").execSync;
 const fs = require("fs");
-const branch = execSync("git symbolic-ref --short HEAD", { encoding: "utf-8" }).trim();
-const branchReg = /(master|hotfix)/g;
+const branch = execSync("git symbolic-ref --short HEAD", {
+  encoding: "utf-8",
+}).trim();
+const blue='\x1B[36m%s\x1B[0m';
+const red='\x1B[31m%s\x1B[0m'
+const branchReg = /(master|hotfix|main)/g;
 if (branchReg.test(branch)) {
+  console.log(blue,"当前分支" + branch);
   const result = execSync("git diff --name-only --cached", {
     encoding: "utf-8",
   });
   const fileArr = result.split("\n");
-
+  console.log(fileArr)
   for (let file of fileArr) {
     if (file.trim()) {
       let reg = /\.json$/g;
       if (!reg.test(file)) {
         var content = fs.readFileSync(file, "utf-8");
         if (content.indexOf("eruda") > -1) {
-          console.error(`${branch}分支${file}包含eruda，请删除后再提交`);
+          console.error(red,`${branch}分支${file}包含eruda，请删除后再提交`);
           throw new Error();
         }
       }
